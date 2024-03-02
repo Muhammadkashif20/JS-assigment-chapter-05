1132116f1bdf2b92ae989218a69ad6df48f68b42
muhammadkashif20 <kashif28@gmail.com>
2024-03-02 00:27:38 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS-assigment-chapter-05
+ JS-assigment-chapter-Math Expressions Familiar Operators
---
+++ app.js
@@ -0,0 +1,107 @@
+// Question 01 :- Greet User:
+// var value; //declear
+// var value = 123 //initialize
+// value=678 //update 
+
+
+var num=3
+var num2=5
+var total=num+num2
+document.write("Sum of 3 and 5 is " +total)
+document.write("<br>")
+var num=3
+var num2=5
+var total=num-num2
+
+document.write("Subtraction of 3 and 5 is " +total)
+document.write("<br>")
+var num=3
+var num2=5
+var total=num*num2
+
+document.write("Multiply of 3 and 5 is " +total)
+document.write("<br>")
+var num=3
+var num2=5
+var total=num/num2
+
+document.write("Divide of 3 and 5 is " +total)
+document.write("<br>")
+var num=3
+var num2=5
+var total=num%num2
+
+document.write("Modulus of 3 and 5 is " +total)
+
+document.write("<br>")
+var expression ="kashif";
+document.write(expression)
+
+// Question 03 :- Display message:
+document.write("<br>")
+var value=5;
+document.write(value)
+document.write("<br>")
+var change=++value 
+document.write(change)
+document.write("<br>")
+change=change+7
+document.write(change)
+--change
+document.write("<br>")
+document.write(change)
+document.write("<br>")
+var divide=change%3
+document.write(divide)
+
+// Question 03 :- film cost
+document.write("<br>")
+var movie=600;
+document.write("Total cost to buy 5 tickets to a movie is "+movie*5 +" PKR")
+// Question 03 :- Table
+
+document.write("<br>")
+document.write("<br>")
+document.write("Table of ")
+var table=4
+document.write(table)
+document.write("<br>")
+document.write("<br>")
+var table=2
+document.write("2 *1" + " =2")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("Error! Please enter a valid password.")
+
