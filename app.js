c2d7508bbd71410a832fdcfa37330da667476a4b
muhammadkashif20 <kashif28@gmail.com>
2024-03-06 12:36:53 -0800

    assignment 5 completed js

--- app.js
+++ app.js
@@ -1,107 +1,183 @@
 // Question 01 :- Greet User:
 // var value; //declear
 // var value = 123 //initialize
-// value=678 //update 
+// value=678 //update
 
+var num = 3;
+var num2 = 5;
+var total = num + num2;
+document.write("Sum of 3 and 5 is " + total);
+document.write("<br>");
+var num = 3;
+var num2 = 5;
+var total = num - num2;
 
-var num=3
-var num2=5
-var total=num+num2
-document.write("Sum of 3 and 5 is " +total)
-document.write("<br>")
-var num=3
-var num2=5
-var total=num-num2
+document.write("Subtraction of 3 and 5 is " + total);
+document.write("<br>");
+var num = 3;
+var num2 = 5;
+var total = num * num2;
 
-document.write("Subtraction of 3 and 5 is " +total)
-document.write("<br>")
-var num=3
-var num2=5
-var total=num*num2
+document.write("Multiply of 3 and 5 is " + total);
+document.write("<br>");
+var num = 3;
+var num2 = 5;
+var total = num / num2;
 
-document.write("Multiply of 3 and 5 is " +total)
-document.write("<br>")
-var num=3
-var num2=5
-var total=num/num2
+document.write("Divide of 3 and 5 is " + total);
+document.write("<br>");
+var num = 3;
+var num2 = 5;
+var total = num % num2;
 
-document.write("Divide of 3 and 5 is " +total)
-document.write("<br>")
-var num=3
-var num2=5
-var total=num%num2
+document.write("Modulus of 3 and 5 is " + total);
 
-document.write("Modulus of 3 and 5 is " +total)
-
-document.write("<br>")
-var expression ="kashif";
-document.write(expression)
+document.write("<br>");
+var expression = "kashif";
+document.write(expression);
 
 // Question 03 :- Display message:
-document.write("<br>")
-var value=5;
-document.write(value)
-document.write("<br>")
-var change=++value 
-document.write(change)
-document.write("<br>")
-change=change+7
-document.write(change)
---change
-document.write("<br>")
-document.write(change)
-document.write("<br>")
-var divide=change%3
-document.write(divide)
+document.write("<br>");
+var value = 5;
+document.write(value);
+document.write("<br>");
+var change = ++value;
+document.write(change);
+document.write("<br>");
+change = change + 7;
+document.write(change);
+--change;
+document.write("<br>");
+document.write(change);
+document.write("<br>");
+var divide = change % 3;
+document.write(divide);
 
 // Question 03 :- film cost
-document.write("<br>")
-var movie=600;
-document.write("Total cost to buy 5 tickets to a movie is "+movie*5 +" PKR")
+document.write("<br>");
+var movie = 600;
+document.write(
+  "Total cost to buy 5 tickets to a movie is " + movie * 5 + " PKR"
+);
 // Question 03 :- Table
 
-document.write("<br>")
-document.write("<br>")
-document.write("Table of ")
-var table=4
-document.write(table)
-document.write("<br>")
-document.write("<br>")
-var table=2
-document.write("2 *1" + " =2")
+document.write("<br>");
+document.write("<br>");
+document.write("Table of ");
+var table = 2;
+document.write(table);
+document.write("<br>");
+document.write("<br>");
+
+document.write("2 *1" + " =2");
+document.write("<br>");
+document.write("2* 2= " + 2 * 2);
+document.write("<br>");
+document.write("2 *3= " + 2 * 3);
+document.write("<br>");
+document.write("2 *4= " + 2 * 4);
+document.write("<br>");
+document.write("2 *5= " + 2 * 5);
+document.write("<br>");
+document.write("2 *6= " + 2 * 6);
+document.write("<br>");
+document.write("2 *7= " + 2 * 7);
+document.write("<br>");
+document.write("2 *8= " + 2 * 8);
+document.write("<br>");
+document.write("2 *9= " + 2 * 9);
+document.write("<br>");
+document.write("2 *10= " + 2 * 10);
+
+document.write("<br>");
+var Celsius_temperature;
+Fahrenheit = "NNoC is NNoF.";
+// document.write()
+var Fahrenheit_temperature;
+Celsius = "NNoF is NNoC.";
+document.write("<br>");
+document.write('Shopping Cart')
+document.write("<br>");
+document.write("<br>");
 
 
+var Priceofitem1='Price of item 1 is '+ 650;
+document.write(Priceofitem1)
+document.write("<br>");
+var Orderedquantityofitem1='Ordered quantity of item 1 '+'is 3';
+document.write(Orderedquantityofitem1)
 
 
+document.write("<br>");
+var Priceofitem2='Price of item 2 is '+100;
+document.write(Priceofitem2)
 
 
+document.write("<br>");
+var quantityofitem2='quantity of item 2 is '+7;
+document.write(quantityofitem2)
 
 
+document.write("<br>");
+var Shippingcharges='Shippingcharges '+100;
+document.write(Shippingcharges)
 
 
+document.write("<br>");
+document.write("<br>");
+document.write('Total Cost of your order is '+2750)
 
 
+document.write("<br>");
+document.write("<br>");
+document.write("Marks_Sheet");
+document.write("<br>");
+document.write("<br>");
+var TotalMark ="Total Mark: "+980;
+document.write(TotalMark)
+document.write("<br>");
+var MarksObtained="Marks Obtained: "+804
+document.write(MarksObtained);
+document.write("<br>");
+var Percentage ="Percentage: "+980%804;
+document.write(Percentage);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write('Currency in PKR');
+document.write("<br>");
+document.write("<br>");
+var Us$_Saudi=104.80*10+28*25 ;
+document.write("Total Currency in PKR"+Us$_Saudi)
 
+document.write("<br>");
+document.write('Question 10 Program Initialize' )
+document.write("<br>");
+var arithmetic=10+5*10
+document.write("Perform all calculations in question 10; " +arithmetic/2)
+document.write("<br>");
+document.write("<br>");
+// document.write("<br>");
+document.write('Age Calculator');
+document.write("<br>");
+document.write("<br>");
+document.write('Current Year: 2024 ');
+var year=2024;
+document.write("<br>");
+document.write('Birth Year: 2008 ');
+var birth=2008;
+document.write("<br>");
+var age=2024%2008;
+document.write('Your age is: '+age)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// alert("Error! Please enter a valid password.")
+document.write("<br>");
+document.write("<br>");
+document.write('<h1>The Geomatrizer ???</h1>')
+document.write("<br>");
+document.write('<H1>The Lifetime Supply Calculator</H1>')
+var snacks='lays'
+var current_Age=16
+var maximum_Age=50
+var max_Amount=30
+document.write('you will need so '+current_Age-maximum_Age%max_Amount)
 
